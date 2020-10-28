8e1789c63b27402ca5ca05495cbf11286ccdd6ca
irempamukcu <73612106+irempamukcu@users.noreply.github.com>
2020-10-28 22:48:05 +0300

    Add files via upload

---
+++ faktoriyel.py
@@ -0,0 +1,24 @@
+print("""
+************************
+Faktörüyel Bulma Programı
+
+Çıkmak İçin X'e Basınız.
+
+************************
+
+""")
+
+while True:
+    sayı = input("Sayı:")
+    if(sayı == "X" or sayı == "x"):
+        print("Program Sonlandırılıyor...")
+        break
+
+    else:
+        sayı = int(sayı)
+
+        faktoriyel = 1
+
+        for i in range(2,sayı+1):
+            faktoriyel *= i
+        print(faktoriyel)
